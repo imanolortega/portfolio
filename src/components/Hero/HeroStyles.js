03e45759dd924cc224f2cf44e72fc3b689f61854
imanolrtega <imanolrtega@gmail.com>
2021-07-05 21:19:26 -0300

    padding fixed

--- src/components/Hero/HeroStyles.js
+++ src/components/Hero/HeroStyles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const SectionHero = styled.section`
   display: ${(props) => (props.grid ? "grid" : "flex")};
   flex-direction: ${(props) => (props.row ? "row" : "column")};
-
+  padding-top: 80px;
   margin: 0 auto;
   max-width: 1040px;
   box-sizing: content-box;
